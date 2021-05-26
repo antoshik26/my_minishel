39a953f58fe889334d03553006fd2afe4b86a706
Lynne Bones <lbones@oa-n1.msk.21-school.ru>
2021-05-26 17:32:57 +0300

    | ; done < aalmost done(not working: when < stat of the line(ex:<file),command after < wrong(ex:gh<file),> >> in progress

--- execve.c
+++ execve.c
@@ -36,6 +36,8 @@ t_command_and_flag *number_of_pipes(int *size,t_command_and_flag *head)
 {
 	//1 | 2 > 3 >> 4 < 5; 6 eof
 	*size=0;
+	if(head->pape==2)
+		*size=-1;
 	if(!head->f_error)
 		head=head->next;
 	else
@@ -61,17 +63,17 @@ int **make_pipe(int size)
 	int i;
 
 	i = 0;
-	if(size == 0)
+	if(size < 0)
 		return(0);
-	pipe = malloc(sizeof(int*) * (size + 1));
+	pipe = malloc(sizeof(int*) * (size + 2));
 	pipe[i++] = NULL;
-	while(i < size)
+	while(i <= size)
 		pipe[i++] = malloc(sizeof(int) * 2);
-	pipe[size]=NULL;
+	pipe[size+1]=NULL;
 	return(pipe);
 	
 }
-void find_function(int size,t_minishell *all,t_command_and_flag *head)
+void find_function(int size,char **env,t_command_and_flag *head)
 {
 	int **pipe;
 	int i;
@@ -80,81 +82,134 @@ void find_function(int size,t_minishell *all,t_command_and_flag *head)
 	i = -1;
 	fd = 0;
 	pipe = make_pipe(size);
-	while(++i<size)
+	while(++i<=size)
 	{
-		if(head->pape==2)//add function check pipe to chosw correct options for open
+		if(head->pape==4)//add function check pipe to chosw correct options for open
 		{
 			fd=open(head->array_flags[1],O_WRONLY);
 			head=head->next;
 		}
-		test(head,pipe[i],pipe[i+1],fd,all->env);
+		test(head,pipe[i],pipe[i+1],fd,env);
 		fd = 0;
 		head=head->next;
 	}
 	while(size--)
-		wait(0);//free pipe;
+		wait(0);
 	if(i>0)
 	{
 		i = 0;
-		free(mas[i++]);
-		while(mas[i])
-			free(mas[i++]);
-		free(mas[i]);
-		free(mas);
+		free(pipe[i++]);
+		while(pipe[i])
+			free(pipe[i++]);
+		free(pipe[i]);
+		free(pipe);
 	}
 }
+char	*ftstrdup(const char *s1)
+{
+	char	*copy_s1;
+	size_t	len;
+	size_t	i;
 
-void functions_launch(t_minishell *all)
+	len = 0;
+	i = 0;
+	while (s1[len] != '\0')
+	{
+		len++;
+	}
+	copy_s1 = (char *)malloc(sizeof(char) * (len + 1));
+	if (copy_s1 == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		copy_s1[i] = s1[i];
+		i++;
+	}
+	copy_s1[i] = '\0';
+	return (copy_s1);
+}
+void functions_launch(t_command_and_flag **head,char **env)
 {
 	t_command_and_flag *current_head;
 	t_command_and_flag *tmp;
 	int size;
 
-	current_head=all->head;
+	current_head=*head;
 	while(current_head)
 	{
 		tmp = number_of_pipes(&size,current_head);
-		//printf("%d %s\n",size,current_head->command);
-		if(size==-1)
+		//printf("%d %s %p\n",size,current_head->command,tmp);
+		if(size==-1 && tmp->f_error)
 			printf("zsh: command not found:%s\n",current_head->command);
-		else
-			find_function(size,all,current_head);
+/*		else if(current_head->pape==4)
+		{
+			do_redirect()
+
+		}*/
+		else if(size>=0)
+			find_function(size,env,current_head);
 		current_head=tmp;
 	}
 }
-int main()
+
+int main(int argc,char **argv,char **env)
 {
 	//function_lounch test
-	/*t_command_and_flag *head1;
+	//t_minishell *all;
+	argc=0;
+	printf("%s\n\n",argv[0]);
+	//free(argv[0]);
+	//char *a[2];
+	//a[1] = argv[0];
+	//a[2]=NULL;
+	t_command_and_flag *head1;
 	t_command_and_flag *head2;
 	t_command_and_flag *head3;
 	t_command_and_flag *head4;
-
+	//all=malloc(sizeof(t_minishell));
 	head1 = malloc(sizeof(t_command_and_flag));
 	head2 = malloc(sizeof(t_command_and_flag));
 	head3 = malloc(sizeof(t_command_and_flag));
 	head4 = malloc(sizeof(t_command_and_flag));
-	head1->command=ftstrdup("wc");
+	head1->command=ftstrdup("/usr/bin/wc");
 	head1->next=0;
 	head1->pape=1;
 	head1->f_error=0;
-	head2->command=ft_strdup("head");
+	head2->command=ftstrdup("/bin/ls");
 	head2->next=0;
-	head2->pape=1;
-	head2->f_error=1;
-	head3->command=ft_strdup("yes");
+	head2->pape=5;
+	head2->f_error=0;
+	head3->command=ftstrdup("/bin/ls");
 	head3->next=0;
 	head3->pape=1;
 	head3->f_error=0;
 	head4->command=NULL;
+	//char a[3];
+	head4->array_flags=malloc(sizeof(3));
+	head4->array_flags[0]=argv[0];
+	head4->array_flags[1]="k";
+	head4->array_flags[2]=0;
 	head4->next=0;
 	head4->pape=4;
 	head4->f_error=0;
-	head1->next=head2;
+	head4->next=head2;
 	head2->next=head3;
-	head3->next=head4;
-	functions_launch(&head1);
+	//head2->next=head3;
+	head1->array_flags=argv;
+	head2->array_flags=argv;
+	head3->array_flags=argv;
+	//head3->next=head4;
+	functions_launch(&head4,env);
 	//make_pipe test
+	//int pipe1[2];
+	//int pipe2[2];
+
+	//test(head1,0,pipe1,0,env);
+	//test(head2,pipe1,pipe2,0,env);
+//	test(head3,pipe2,0,0,env);
+//	wait(0);
+//	wait(0);
+//	wait(0);
 }
 
 
