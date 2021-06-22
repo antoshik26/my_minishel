357919810b8a4fa857a7da2ef1db65acd2a76801
Denisse Madeleine <dmadelei@pl-k1.msk.21-school.ru>
2021-06-22 21:34:20 +0300

    not wotch

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,87 +3,95 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbones <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: dmadelei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/24 17:58:20 by lbones            #+#    #+#             */
-/*   Updated: 2021/04/30 18:04:46 by lbones           ###   ########.fr       */
+/*   Created: 2020/12/18 17:04:51 by dmadelei          #+#    #+#             */
+/*   Updated: 2020/12/18 17:04:57 by dmadelei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_free(char **c)
+static int		wordnum(char const *s, char c)
 {
-	int	i;
+	int word;
+	int cnt;
 
-	i = 0;
-	while (c[i])
-		free(c[i++]);
-	free(c);
+	word = 0;
+	cnt = 0;
+	while (s[cnt] != '\0')
+	{
+		if (s[cnt] != c && (s[cnt + 1] == c || s[cnt + 1] == '\0'))
+			word++;
+		cnt++;
+	}
+	return (word);
 }
 
-int	ft_len(int i, const char *s, char c)
+static int		wordlen(char const *s, char c)
 {
-	int	len;
-	int	i3;
+	int cnt;
+	int len;
 
-	i3 = i;
+	cnt = 0;
 	len = 0;
-	while (s[i])
+	while (s[cnt] != c && s[cnt] != '\0')
 	{
-		while (s[i] != c && s[i])
-			i++;
-		if ((len == 0 || i != i3) || s[i])
-			len++;
-		while (s[i] == c && s[i])
-			i++;
+		cnt++;
+		len++;
 	}
 	return (len);
 }
 
-char	**ft_fill(const char *s, char c, int i, char **s1)
+static void		*free_mem(char **result)
 {
-	int	i3;
-	int	len;
-	int	i1;
+	int cnt;
 
-	i1 = 0;
-	i3 = 0;
-	len = ft_len(i, s, c);
-	while (len > i1)
+	cnt = 0;
+	while (result[cnt])
 	{
-		i = i3;
-		while (s[i++] == c)
-			i3++;
-		while (s[i] != c && s[i])
-			i++;
-		s1[i1] = ft_substr(s, i3, i - i3);
-		if (!s1[i1])
-		{
-			ft_free(s1);
-			return (NULL);
-		}
-		i3 = i;
-		i1++;
+		free(result[cnt]);
+		cnt++;
 	}
-	return (s1);
+	free(result);
+	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+static char		**write_in(char const *s, int word, char c, char **result)
 {
-	int		i;
-	int		len;
-	char	**s1;
+	int dim1;
+	int dim2;
+	int len;
 
-	i = 0;
+	dim1 = 0;
+	while (dim1 < word)
+	{
+		while (*s == c && *s != '\0')
+			s++;
+		len = wordlen(s, c);
+		result[dim1] = (char*)malloc(sizeof(char) * (len + 1));
+		if (result[dim1] == NULL)
+			return (free_mem(result));
+		dim2 = 0;
+		while (dim2 < len)
+			result[dim1][dim2++] = *s++;
+		result[dim1][dim2] = '\0';
+		dim1++;
+	}
+	result[dim1] = NULL;
+	return (result);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	char	**result;
+	int		word;
+
 	if (!s)
 		return (NULL);
-	while (s[i] == c && s[i])
-		i++;
-	len = ft_len(i, s, c);
-	s1 = (char **)malloc(sizeof(char *) * (len + 1));
-	if (!s1)
+	word = wordnum(s, c);
+	if (!(result = (char**)malloc(sizeof(char*) * (word + 1))))
 		return (NULL);
-	s1[len] = NULL;
-	return (ft_fill(s, c, i, s1));
+	result = write_in(s, word, c, result);
+	return (result);
 }
